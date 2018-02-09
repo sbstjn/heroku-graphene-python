a6403e2e4063737217f01a011ec018bcecb0c8b2
sbstjn <mail@sbstjn.com>
2018-02-09 17:33:03 +0100

    Update Pipfile

--- main.py
+++ main.py
@@ -3,17 +3,10 @@
 import graphene
 import requests
 import os
-import json as jsonRaw
+import json
 
 from sanic import Sanic
-from sanic.response import json
 from sanic_graphql import GraphQLView
-
-def _json_object_hook(d):
-    return namedtuple('X', d.keys())(*d.values())
-
-def json2obj(data):
-    return json.loads(data, object_hook=_json_object_hook)
 
 class Person (graphene.ObjectType):
     id = graphene.ID()
@@ -26,13 +19,13 @@ class Query(graphene.ObjectType):
 
     def resolve_person(self, info, id):
         r = requests.get("https://to23rx1sik.execute-api.us-west-1.amazonaws.com/Prod/person/" + id)
-        data = jsonRaw.loads(r.content)
+        data = json.loads(r.content)
         person = data['data']
 
         return Person(id=person['id'], name=person['name'], age=person['age'])
     def resolve_people(self, info):
         r = requests.get("https://to23rx1sik.execute-api.us-west-1.amazonaws.com/Prod/people")
-        data = jsonRaw.loads(r.content)
+        data = json.loads(r.content)
 
         people = []
         for person in data['data']:
